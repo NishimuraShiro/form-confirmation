5d41a73325351e8b38fcfca88b19168bfdb84c37
HA-mi-lton <hamiltonshiro3@gmail.com>
2023-09-13 14:18:41 +0900

    Next13のLinkタグと@12との変更点

--- form-confirmation/src/app/page.tsx
+++ form-confirmation/src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>テストです</div>;
+  return (
+    <div className="text-5xl flex flex-col items-center justify-center min-h-screen">
+      {/* Next.js@12 */}
+      {/* <Link href={"form"}>
+        <a>Formぺーじへ飛ぶ</a>
+      </Link> */}
+      {/* Next.js@13 */}
+      <Link href={"form"}>Formぺーじへ飛ぶ</Link>
+    </div>
+  );
 }
