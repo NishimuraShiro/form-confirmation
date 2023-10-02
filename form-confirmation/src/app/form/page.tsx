c557174cb7d0f4f10bf073a4c1200728592beec8
HA-mi-lton <hamiltonshiro3@gmail.com>
2023-10-02 19:03:28 +0900

    一旦プッシュ

--- form-confirmation/src/app/form/page.tsx
+++ form-confirmation/src/app/form/page.tsx
@@ -6,7 +6,7 @@ import React, {
   FormEvent,
   useRef,
   useState,
-  useEffect,
+  useEffect
 } from "react";
 
 interface FormData {
@@ -17,36 +17,42 @@ interface FormData {
 const Form = () => {
   const [formData, setFormData] = useState<FormData>({
     name: "",
-    mail: "",
+    mail: ""
   });
+
+  const [name, setName] = useState<string>("");
+  const [mail, setMail] = useState<string>("");
   const searchParams = useSearchParams();
   const paramsName = searchParams.get("name") || "";
   const paramsMail = searchParams.get("mail") || "";
-  const [editData, setEditData] = useState({
-    paramsName: paramsName,
-    paramsMail: paramsMail,
-  });
+
   const router = useRouter();
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    // console.log(value);
+    setFormData({ name: value, mail: formData.mail });
+    // setName(mail);
   };
 
   const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-
-    const formDataParams = new URLSearchParams();
-    formDataParams.append("name", formData.name);
-    formDataParams.append("mail", formData.mail);
-    router.push(`/form/confirmation/?${formDataParams.toString()}`);
+    // e.preventDefault();
+    // const formDataParams = new URLSearchParams();
+    // formDataParams.append("name", formData.name);
+    // formDataParams.append("mail", formData.mail);
+    // console.log(formData);
+    // router.push(`/form/confirmation?${formDataParams.toString()}`);
   };
 
-  const nameInputRef = useRef<HTMLInputElement | null>(null);
-  useEffect(() => {
-    if (nameInputRef) {
-      nameInputRef.current?.focus();
-    }
-  }, [nameInputRef]);
+  // const nameInputRef = useRef<HTMLInputElement | null>(null);
+  // useEffect(() => {
+  //   if (nameInputRef) {
+  //     nameInputRef.current?.focus();
+  //   }
+  // }, [nameInputRef]);
+
+  const nameInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
 
   return (
     <>
@@ -60,18 +66,30 @@ const Form = () => {
           <input
             type="text"
             name="name"
-            value={formData.name}
+            value={name}
             onChange={handleInputChange}
-            ref={nameInputRef}
+            // ref={nameInputRef}
           />
         </div>
         <div className="flex pb-7 text-xl">
           <p>メールアドレス：</p>
+          <p>{formData.name}</p>
           <input
             type="text"
             name="mail"
             value={formData.mail}
             onChange={handleInputChange}
+            // ref={nameInputRef}
+          />
+        </div>
+
+        <div className="flex pb-7 text-xl">
+          <p>名前：</p>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={nameInputHandler}
             // ref={nameInputRef}
           />
         </div>
